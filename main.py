0ef31458b471d9c93c8fc33533bb74bcea6a03f8
Arnold Xiang <anuoxiang@163.com>
2015-12-22 17:10:53 +0800

    基本完整

--- main.py
+++ main.py
@@ -6,112 +6,206 @@ import sys
 import os
 import picamera
 import io
-# import yuv2rgb
 import zbarlight
 from PIL import Image
-from escpos import *        # 打印机
-import re                   # 正则表达式
-import urllib2              # 网络访问
-import json                 # JSON解析
+from escpos import *  # 打印机
+import re  # 正则表达式
+import urllib2  # 网络访问
+import json  # JSON解析
 from datetime import datetime  # 日期处理
-import time                 # 多线程控制
+import time  # 多线程控制
 import thread
+
+
+class TxtInfo:
+    txtDisplayDelay = 5
+    def __init__(self, name):
+        self.name = name
+        self.initTime = time.time()
+
+    def isDone(self):
+        return time.time() - self.initTime > txtDisplayDelay
+
+
+# 摄像头预览图
+def GetPrevImg():
+    stream = io.BytesIO()  # Capture into in-memory stream
+    camera.capture(stream, use_video_port=True, format='jpeg')
+    stream.seek(0)
+    image = Image.open(stream)
+    image.load()
+    stream.readinto(rgb)
+    stream.close()
+    return image
+
+
+# 二维码扫描
+def ScanCode(image):
+    txt = None
+    codes = zbarlight.scan_codes('qrcode', image)
+    if codes != None:
+        txt = codes[0].decode('utf-8')
+    return txt
+
+
+# 网络验证
+
+
+# 打印
+def Print():
+    Echo("准备打印...")
+    usb = printer.Usb(0x0416, 0x5011, 0, out_ep=0x01)
+
+    Echo("打印...")
+    usb.image('logo.bmp')
+    usb.set('CENTER', 'B', 'B', 1, 2)
+    usb.text(u"上海第一八佰伴\n\n".encode('gbk'))
+    usb.text(u"岁末疯抢\n\n".encode('gbk'))
+    #内容正文开始
+    usb.set('LEFT', 'B', 'B', 1, 1)
+    usb.text(u"老凤祥50g金条【预约券】".encode('gbk') + "\n")
+
+    usb.set('LEFT', 'A', 'A', 1, 1)
+    usb.text(u"抢购时间:%s\n".encode('gbk') % datetime.now().strftime("%m/%d %H:%M:%S"))
+
+    '''
+    usb.set('LEFT', 'B', 'A', 1, 1)
+    usb.text(u"交易账户：".encode('gbk'))
+    usb.set('LEFT', 'B', 'B', 1, 1)
+    usb.text(u"测试账户\n".encode('gbk'))
+    '''
+    usb.text("\n\n")
+    usb.set('CENTER', '', '', 1, 1)
+    usb.text(u"校验码\n".encode('gbk'))
+    # usb.barcode('9787900420206', 'EAN13', 64, 2, '', '')
+    usb.qr('52588803')
+    usb.text("\n\n")
+
+    usb.set('LEFT', 'A', 'A', 1, 1)
+    usb.text(u"    凭本券当场换领相应品牌的销售通知单，本券仅限当场兑换有效，如不兑换视为自动放弃，本券作废。\n\n".encode('gbk'))
+
+    usb.set('CENTER', '', '', 1, 1)
+    usb.text(u"技术支持\n坤鼎文化发展有限公司".encode('gbk') + "\n")
+
+    usb.set('CENTER', '', '', 1, 1)
+    usb.text(u"2016".encode('gbk') + "\n")
+
+    usb.text("------------------------------\n")
+    #
+
+    usb.cut('full')
+
+
+def Echo(content):
+    infos.append(TxtInfo(content))
+    Display()
+
+def Display():
+    for info in infos:
+        label = myfont.render(info.name, 1, (255, 255, 0))
+        screen.blit(label,
+                    ((width - label.get_width()) / 2,
+                     txtInfoTop + label.get_height()*infos.index(info)))
+
+    #过期的移走
+    for info in infos:
+        if info.isDone:
+            infos.remove(info)
+
+
 
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 # os.putenv('SDL_FBDEV'      , '/dev/fb1')
 
-# main progress
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
-rgb = bytearray(320 * 240 * 3)
-yuv = bytearray(320 * 240 * 3 / 2)
 
+# 界面初始化
 pygame.init()
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/mouse0')
-size = width, height = 1024, 600
-speed = [2, 2]
-black = 0, 0, 0
-
+size = width, height = 600, 1024
 screen = pygame.display.set_mode(size)
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-camera.rotation = 180
-camera.crop = (0.0, 0.0, 1.0, 1.0)
+# screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 myfont = pygame.font.SysFont("wenquanyizenhei", 30)
 sizeData = [  # Camera parameters for different size settings
               # Full res      Viewfinder  Crop window
               [(2592, 1944), (320, 240), (0.0, 0.0, 1.0, 1.0)],  # Large
               [(1920, 1080), (320, 180), (0.1296, 0.2222, 0.7408, 0.5556)],  # Med
-              [(1440, 1080), (320, 240), (0.2222, 0.2222, 0.5556, 0.5556)]]  # Small
+              [(1440, 1080), (320, 320), (0.2222, 0.2222, 0.5556, 0.5556)]]  # Small
 sizeMode = 2
-
-ball = pygame.image.load('ball.bmp')
-ballrect = ball.get_rect()
+txtInfoTop = 100        #从y坐标哪里开始显示
+txtDisplayDelay = 5     #提示信息显示5秒
 # pygame.mouse.set_visible(False)
-bg = pygame.image.load('bg.jpg')
-pygame.mouse.set_pos(512, 300)
+
+# 摄像头的定义
+rgb = bytearray(sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)
+camera = picamera.PiCamera()
+camera.resolution = sizeData[sizeMode][1]
+camera.rotation = 180
+camera.crop = sizeData[sizeMode][2]
+MaxWaitingTime = 30  # 30秒摄像头启动等待
+begTimer = 0
+
+
+# 按钮定义
+speed = [0, -1]
+button = pygame.image.load('exchange.png')
+buttonOrgPos = {"x": 135, "y": 577}# 原始位置
+buttonrect = button.get_rect()
+buttonrect.x = buttonOrgPos["x"]
+buttonrect.y = buttonOrgPos["y"]
+bg = pygame.image.load('home.jpg')
+pygame.mouse.set_pos(300, 512)
 pos = None
+
+# 信息提示
+infos = []
+
+
+runCamera = False
 while 1:
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    # 背景绘图
+    screen.blit(bg, (0, 0))
+
+    buttonrect = buttonrect.move(speed)
+    if buttonrect.top < buttonOrgPos["y"] - 30 or buttonrect.top > buttonOrgPos["y"]:
         speed[1] = -speed[1]
 
+    label = None
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             camera.close()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            if ((pos[0] > ballrect.left) and (pos[0] < ballrect.right) and
-                    (pos[1] > ballrect.top) and (pos[1] < ballrect.bottom)):
-                speed = [speed[0] + 1, speed[1] + 1]
-                # speed = [3, 3]
+            # label = myfont.render("click!", 1, (255, 255, 0))
+            if (not runCamera and (pos[0] > buttonrect.left) and (pos[0] < buttonrect.right) and
+                    (pos[1] > buttonrect.top) and (pos[1] < buttonrect.bottom)):
+                runCamera = True
+                begTimer = time.time()
+            else:
+                runCamera = False
+                Echo(u"点我呀~~~")
 
-    screen.fill(black)
-    screen.blit(bg, (0, 0))
-    screen.blit(ball, ballrect)
-    # display camera
-    stream = io.BytesIO()  # Capture into in-memory stream
-    camera.capture(stream, use_video_port=True, format='jpeg')
-    stream.seek(0)
-    image = Image.open(stream)
-    image.load()
-    # stream.readinto(yuv)  # stream -> YUV buffer
-    stream.readinto(rgb)
-    stream.close()
-    mode = image.mode
-    size = image.size
-    data = image.tobytes()
-    img = pygame.image.fromstring(data, size, mode)
-    # yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0],sizeData[sizeMode][1][1])
-    # img = pygame.image.frombuffer(rgb,(320,240),'RGB')
-    '''img = pygame.image.frombuffer(rgb[0:
-      (sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)],
-      sizeData[sizeMode][1], 'RGB')
-	'''
-    if img:
-        screen.blit(img, ((1024 - img.get_width()) / 2, (600 - img.get_height()) / 2))
-        codes = zbarlight.scan_codes('qrcode', image)
-        if codes != None:
-            print type(codes)
-            txt = codes[0].decode('utf-8')
-            label = myfont.render(txt, 1, (255, 255, 0))
+    if time.time() - begTimer > MaxWaitingTime:
+        runCamera = False
+    if label != None:
+        screen.blit(label, ((width - label.get_width()) / 2, 100))
 
-            screen.blit(label, ((1024 - label.get_width()) / 2, 100))
-            usb = printer.Usb(0x0416, 0x5011, 0, out_ep=0x01)
-            usb.set('CENTER', 'B', 'B', 1, 2)
-            usb.text(txt.encode('gbk'))
+    if not runCamera:
+        screen.blit(button, buttonrect)
+    else:
+        image = GetPrevImg()
+        img = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
 
-            # if (qr.decode(img)):
-            #	label = myfont.render(qr.decode(img).data, 1, (255,255,0))
-            #	screen.blit(label, (100, 100))
-    if (pos != None):
-        pygame.draw.circle(screen, BLUE, pos, 20, 0)
+        if img:
+            # 摄像头预览图的位置
+            screen.blit(img, ((width - img.get_width()) / 2, (height - img.get_height()) / 2 + 200))
+            # 扫码读取
+            txt = ScanCode(image)
+            if txt != None:
+                Print()
+
+    Display()
+
     pygame.display.flip()
